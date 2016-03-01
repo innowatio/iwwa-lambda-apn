303a6f8f377ca92e39d3f346a2ca6c2a898a6a2d
Paolo Scanferla <paolo.scanferla@mondora.com>
2016-03-01 12:24:50 +0100

    Parsed TRANSMISSION_WAIT_TIME into int

--- src/config.js
+++ src/config.js
@@ -4,6 +4,10 @@ dotenv.config();
 
 export const APN_CERTIFICATE = process.env.APN_CERTIFICATE;
 export const APN_KEY = process.env.APN_KEY;
-export const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/test";
-export const TRANSMISSION_WAIT_TIME = process.env.TRANSMISSION_WAIT_TIME || 2000;
+export const MONGODB_URL = (
+    process.env.MONGODB_URL || "mongodb://localhost:27017/test"
+);
+export const TRANSMISSION_WAIT_TIME = (
+    parseInt(process.env.TRANSMISSION_WAIT_TIME) || 2000
+);
 export const LOG_LEVEL = process.env.LOG_LEVEL || "info";
